7b50c2c5db36422ad237171afb546299076b85d6
Rishabh Verma <aryanverma2412@gmail.com>
2022-01-11 11:56:13 +0530

    Time: 7 ms (57.54%), Space: 16.7 MB (72.35%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
@@ -1,15 +1,18 @@
 
 class Solution {
 public:
-    int func(TreeNode* root,int c_val){
-        if(!root) return 0;
-        c_val=(c_val<<1)|root->val;
-        if(!root->left and !root->right) return c_val;
+//     int func(TreeNode* root,int c_val){
+//         if(!root) return 0;
+//         c_val=(c_val<<1)|root->val;
+//         if(!root->left and !root->right) return c_val;
         
         
-        return func(root->left,c_val)+func(root->right,c_val);
-    }
-    int sumRootToLeaf(TreeNode* root) {
-        return func(root,0);
+//         return func(root->left,c_val)+func(root->right,c_val);
+//     }
+    int sumRootToLeaf(TreeNode* root,int val=0) {
+        if (!root) return 0;
+        val = (val * 2 + root->val);
+        return root->left == root->right ? val :
+        sumRootToLeaf(root->left, val) + sumRootToLeaf(root->right, val);
     }
 };
