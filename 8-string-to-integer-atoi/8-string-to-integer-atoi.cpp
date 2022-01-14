16b0b354cc44f246db613539fa24006c5244bb2c
Rishabh Verma <aryanverma2412@gmail.com>
2022-01-14 14:17:39 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (5.68%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
-    int myAtoi(string s) {
-        string m="";
-        int i =0;
-        while(i<s.size()){
-            if(!m.empty() and s[i]==' ') break;
-            if(s[i]!=' ') m.push_back(s[i]);
-            i++;
-        }
-        if(m=="") return 0;
-        stringstream ss(m);
-        int n;
-        ss>>n;
-        return n;
+    int myAtoi(string str) {
+        int r = 0;
+        stringstream ss;
+        ss << str;
+        ss >> r;
+        return r;
     }
     
 };
