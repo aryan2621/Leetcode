8f00a853ced8c5d3d099f7c39a2e6f3d8cae0d9e
Rishabh Verma <aryanverma2412@gmail.com>
2022-01-15 14:42:55 +0530

    Time: 1020 ms (6.78%), Space: 17.9 MB (5.02%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int solve(vector<int>& nums,vector<int>& dp,int pos){
+        if(pos >= size(nums) - 1) return 0; 
+	if(dp[pos] != 100000) return dp[pos];
+	for(int j = 1; j <= nums[pos]; j++)
+		dp[pos] = min(dp[pos], 1 + solve(nums, dp, pos + j));        
+	return dp[pos];
+    }
+    
+    int jump(vector<int>& nums) {
+        vector<int> dp(size(nums),100000);
+        return solve(nums,dp,0);
+    }
+};
