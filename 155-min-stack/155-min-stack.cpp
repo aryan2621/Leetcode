c53e15946248ea5c78be9f0c141e7f47bb1d3655
Rishabh Verma <aryanverma2412@gmail.com>
2022-01-15 13:06:21 +0530

    Time: 25 ms (41.98%), Space: 16.3 MB (91.62%) - LeetHub

---
+++ 155-min-stack/155-min-stack.cpp
@@ -0,0 +1,23 @@
+class MinStack {
+private:
+    stack<int> s1,s2;
+public:
+
+    void push(int x) {
+        s1.push(x);
+        if(s2.empty() or x<=getMin()) s2.push(x);
+    }
+    
+    void pop() {
+        if (s1.top() == getMin())  s2.pop();
+	    s1.pop();
+    }
+    
+    int top() {
+       return s1.top();
+    }
+    
+    int getMin() {
+        return s2.top();
+    }
+};
