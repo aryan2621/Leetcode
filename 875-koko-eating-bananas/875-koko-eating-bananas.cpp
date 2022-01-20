e9c457af1bc612bdc3f6695e52ad98f6e44059bb
Rishabh Verma <aryanverma2412@gmail.com>
2022-01-20 12:36:39 +0530

    Time: 147 ms (5.10%), Space: 18.8 MB (53.71%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+     int Calculate(vector<int>& nums,int k){
+        int NumberOfHours=0;
+        for(int i=0;i<nums.size();i++){
+            NumberOfHours+=(nums[i]%k==0) ? nums[i]/k : (nums[i]/k)+1;
+        }
+        return NumberOfHours;
+    }
+    int minEatingSpeed(vector<int>& nums, int H) {
+       int left=1;
+        int right=1000000000;
+        int ans=0;
+        while(left<=right){
+           int k=left+(right-left)/2;    
+            
+           int NumberOfHours = Calculate(nums,k);
+            
+            if(NumberOfHours>H){
+                left=k+1;
+            }
+            else if(NumberOfHours<=H){
+                ans=k;
+                right=k-1;
+            }
+        }
+        return ans;
+    }
+};
